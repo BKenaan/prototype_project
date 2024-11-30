7e410c297cf2db9e937c7702072b3ead833cb1c6
majtob <majto11@gmail.com>
2024-11-30 18:03:29 +0000

    On branch addTest  Your branch is ahead of 'origin/addTest' by 3 commits.  Changes to be committed: 	modified:   backend/build.gradle 	modified:   backend/src/test/java/dataModelTest.java

--- backend/src/test/java/dataModelTest.java
+++ backend/src/test/java/dataModelTest.java
@@ -1,3 +1,20 @@
-import static org.junit.jupiter.api.Assertions.assertEquals;
+package test.java;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
+import main.java.DataModel;
+
+public class DataModelTest {
+
+    @Test
+    public void testExample() {
+        // Arrange
+        DataModel.Cart cart = new DataModel.Cart("cart1", "host1", null, null, "Open", 0.0, null);
+        
+        // Act
+        cart.setCartId("test");
+        
+        // Assert
+        assertEquals("test", cart.getCartId());
+    }
+}
